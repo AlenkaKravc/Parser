5d0d5327777e549dc35a7f82680afa10883fd9bd
Alena <kravecas@rbtechnologies.ru>
2019-03-17 03:16:29 +0300

    parsing underline

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 let fs = require('fs');
-let Parser = require('./lib/Parser');
+let MarkdownParser = require('./lib/Parser');
 
 
 let text = fs.readFileSync('file.md', 'utf-8');
 
 
-let parser = new Parser(text);
+let parser = new MarkdownParser(text);
 //parser.printContext();
 //console.log("_____");
 parser.parse();
 //console.log("_____");
 //parser.show();
-console.log("_____ Tree ____");
+console.log("\n            _____ Tree ____\n");
 parser.printTree();
+console.log("\n");
 //parser.printLines();
 
